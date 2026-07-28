9d0b257fdb8c1a211a7aee0abad4f6619429fc19
Maximo Bianco <ninjax06112009@gmail.com>
2026-07-28 12:57:10 -0300

    yqs

--- characters/batguy.gd
+++ characters/batguy.gd
@@ -1,3 +1,4 @@
+
 extends CharacterBody2D
 const SPEED=250
 const JUMP_VELOCITY= -450
@@ -5,25 +6,30 @@ const CROUCH_FREEZE = 0.05
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var is_crouching = false
-
 var standing_cshape = preload("res://resources/bg_standing_cshape.tres")
 var crouching_cshape = preload("res://resources/bg_crouch_cshape.tres")
+
+
 func _physics_process(delta):
 	
 	if not is_on_floor():
 		velocity.y +=gravity*delta
-		$batGuyAnimation.play("jump")
-		$batGuyAnimation.speed_scale = 0
-		$CollisionShape2D.shape = standing_cshape
+		if velocity.y < 0:
+			$batGuyAnimation.play("jump")
+			$batGuyAnimation.speed_scale = 0
+			
+		elif velocity.y > 0 :
+			$batGuyAnimation.speed_scale = 1
+			
 	if Input.is_action_just_pressed("jump") and is_on_floor() and is_crouching==false:
 		velocity.y =JUMP_VELOCITY
 		
-	if Input.is_action_just_pressed("crouch"):
+	if Input.is_action_just_pressed("crouch") and is_on_floor():
 		crouch()
 	elif Input.is_action_just_released("crouch"):
 		stand()
-	if Input.is_action_just_pressed("LP") :
-		LP()
+	if Input.is_action_just_pressed("LP"):
+		$batGuyAnimation.play("5_LP")
 	var direction = Input.get_axis("left","right")
 	if direction and is_crouching == false:
 		velocity.x = direction * SPEED
@@ -47,21 +53,18 @@ func _physics_process(delta):
 				$batGuyAnimation.speed_scale = 1
 	move_and_slide()
 	
+	
 func crouch():
 	if is_crouching:
 		return
 	is_crouching = true
-	
 	$CollisionShape2D.shape = crouching_cshape
-	$CollisionShape2D.position.y = 65
+	$CollisionShape2D.position.y = 57
+	$CollisionShape2D.position.x = -23
 func stand():
 	if not is_crouching:
 		return
 	is_crouching = false
-	
 	$CollisionShape2D.shape = standing_cshape
-	$CollisionShape2D.position.y = 45
-func LP():
-	if is_on_floor() and not is_crouching:
-		$batGuyAnimation.play("5_LP")
-		
+	$CollisionShape2D.position.y = 34
+	$CollisionShape2D.position.x = -12
